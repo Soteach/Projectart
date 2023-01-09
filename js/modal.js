17995036ba1d7faff08a629e7d0b71f340bf46fc
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-01-09 20:31:20 +0200

    fix modal buttons

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    arrOfOpenBtn: document.querySelectorAll('[data-modal-open]'),
+    arrOfCloseBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  [...refs.arrOfOpenBtn].map(elem => {
+    elem.addEventListener('click', toggleModal);
+  });
+
+  [...refs.arrOfCloseBtn].map(elem => {
+    elem.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
